a01dd54a21922364ddc39547a8fdc3a3a50b7346
huertas13 <d.h.montero47@hotmail.com>
2025-04-03 14:35:13 +0200

    actua

--- daw-task/src/main/java/com/daw/services/TareaService.java
+++ daw-task/src/main/java/com/daw/services/TareaService.java
@@ -5,79 +5,90 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.daw.persistence.entities.Tarea;
 import com.daw.persistence.entities.enums.Estado;
 import com.daw.persistence.repositories.TareaRepository;
+import com.daw.services.exceptions.TareaException;
 import com.daw.services.exceptions.TareaNotFoundException;
 
 @Service
 public class TareaService {
-
+	
 	@Autowired
 	private TareaRepository tareaRepository;
-
+	
 	public List<Tarea> findAll(){
+		return this.tareaRepository.findAll();
+	}
 	
-	return this.tareaRepository.findAll();
-
-}
-	
-	public Tarea findById (int idTarea) {
-		
+	public Tarea findById(int idTarea) {
 		if(!this.tareaRepository.existsById(idTarea)) {
-			throw new TareaNotFoundException("El id no existe");
-			
+			throw new TareaNotFoundException("El ID "+idTarea  + " de la tarea no existe. ");
 		}
 		
 		return this.tareaRepository.findById(idTarea).get();
+	}
 	
-	}	
-
-		public boolean existsById(int idTarea) {
-			
-			return this.tareaRepository.existsById(idTarea);
-			
-		}
-		public boolean deleteById (int idTarea) {
-			boolean result = false;
-			
-			if (this.tareaRepository.existsById(idTarea)) {
-				this.tareaRepository.deleteById(idTarea);
-				result = true;
-				
-			}
-			
-			return result;
-			
-		}
-
+	public boolean existsById(int idTarea) {
+		return this.tareaRepository.existsById(idTarea);
+	}
+	
+	public boolean deleteById(int idTarea) {
+		boolean result = false;
 		
-		public Tarea create (Tarea tarea) {
-			
-			tarea.setFechaCreacion(LocalDate.now());
-			tarea.setEstado(Estado.PENDIENTE);
-			
-			return this.tareaRepository.save(tarea);
+		if(this.tareaRepository.existsById(idTarea)) {
+			this.tareaRepository.deleteById(idTarea);
+			result = true;
 		}
 		
-
-		public Tarea update(Tarea tarea) {
+		return result;
+	}
+	
+	public Tarea create(Tarea tarea) {	
+		tarea.setId(0);
+		tarea.setFechaCreacion(LocalDate.now());
+		tarea.setEstado(Estado.PENDIENTE);
+		
+		return this.tareaRepository.save(tarea);
+	}
+	
+	public Tarea update(int idTarea, Tarea tarea) {
+		
+		if(idTarea != tarea.getId()) {
 			
-				Tarea tareaBD = this.findById(tarea.getId());
-				tarea.setFechaCreacion(tareaBD.getFechaCreacion());
-				tarea.setEstado(tareaBD.getEstado());
-				
-				return this.tareaRepository.save(tarea);
-			}
-			
-			
-			
+			throw new TareaException("El ID del path("+ idTarea + ") y el del body(" + tarea.getId()+ ") no coinciden. ");
 		}
 		
+		Tarea tareaBD = this.findById(tarea.getId());
+		tareaBD.setTitulo(tarea.getTitulo());
+		tareaBD.setDescripcion(tarea.getDescripcion());
+		tareaBD.setFechaVencimiento(tarea.getFechaVencimiento());
 		
+		return this.tareaRepository.save(tareaBD);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
-
-
-
+}
--- daw-task/src/main/java/com/daw/web/controllers/TareaController.java
+++ daw-task/src/main/java/com/daw/web/controllers/TareaController.java
@@ -1,8 +1,12 @@
+
 package com.daw.web.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,64 +18,84 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.daw.persistence.entities.Tarea;
 import com.daw.services.TareaService;
+import com.daw.services.exceptions.TareaException;
 import com.daw.services.exceptions.TareaNotFoundException;
 
 @RestController
 @RequestMapping("/tareas")
-
 public class TareaController {
-
+	
 	@Autowired
 	private TareaService tareaService;
 	
-
 	@GetMapping
-		public List<Tarea> list(){
-		return this.tareaService.findAll();
-		
+	public ResponseEntity<List<Tarea>>  list() {
+		return ResponseEntity.status(HttpStatus.OK).body(this.tareaService.findAll());
 	}
 	
 	@GetMapping("/{idTarea}")
-	public Tarea findbyId( @PathVariable int idTarea) {
-		
-		
-		try {
-			return this.tareaService.findById(idTarea);
+	public ResponseEntity<?> findById(@PathVariable int idTarea) {
+		try{
+			return ResponseEntity.ok(this.tareaService.findById(idTarea));
 		}
-	
-		catch(TareaNotFoundException ex) {
-			
+		catch (TareaNotFoundException ex){
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
 		}
-		
 	}
-
-@DeleteMapping("/{idTarea}")
-	public boolean deleteById(@PathVariable int idTarea) {
-	
-		 this.tareaService.deleteById(idTarea);
-		
-		 return this.tareaService.deleteById(idTarea);
-}
-
-@PostMapping
-
-	public Tarea create (@RequestBody Tarea tarea) {
-	
-		return this.tareaService.create(tarea);
 	
 	
-}
-
-
+	@DeleteMapping("/{idTarea}")
+	public ResponseEntity<?> delete(@PathVariable int idTarea) {
+		if(this.tareaService.deleteById(idTarea)) {
+			return ResponseEntity.ok().build();
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El ID indicado no existe");
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tarea> create(@RequestBody Tarea tarea) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.tareaService.create(tarea));
+	}
+	
 	@PutMapping("/{idTarea}")
-	public Tarea update(@PathVariable int idTarea, @RequestBody Tarea tarea) {
-		
-		return this.tareaService.update(tarea);
+	public ResponseEntity<?> update(@PathVariable int idTarea, @RequestBody Tarea tarea) {
+		try {
+			return ResponseEntity.ok(this.tareaService.update(idTarea, tarea));
+		}
+	
+	catch (TareaNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+	
+	catch (TareaException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
 		
 	}
-
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
-
-
-
